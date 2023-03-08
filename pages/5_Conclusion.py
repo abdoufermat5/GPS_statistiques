b6885d2989e3fef56dc7f35073019c208c2492e2
abdoufermat <saabdouyaya@ept.sn>
2023-03-09 00:40:54 +0100

    add global view image

--- pages/5_Conclusion.py
+++ pages/5_Conclusion.py
@@ -1,5 +1,8 @@
+import os
+
 import streamlit as st
-import streamlit.components.v1 as components
+from PIL import Image
+
 from gps_uvsq_utils.st_helpers import load_assets, load_sidebar_footer
 
 st.set_page_config(
@@ -12,6 +15,20 @@ st.set_page_config(
 load_assets()
 load_sidebar_footer("Conclusion", "area_chart")
 st.markdown("""<h2 class="text-center" style="background-color:#cad2e0">Conclusion</h2>""", unsafe_allow_html=True)
+
+# get path 1 above the current file
+
+above_path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+path_dir = os.path.join(above_path, "html", 'assets', 'architecture.png')
+
+col1, col2, col3 = st.columns([1, 2, 1])
+image = Image.open(path_dir)
+with col1:
+    pass
+with col2:
+    st.image(image, caption='Vue global', use_column_width=True)
+with col3:
+    pass
 
 st.markdown("""
 <br/>
